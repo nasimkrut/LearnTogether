917422b373ae7e8de2053e5aabd712e45abee440
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2024-12-04 23:13:34 +0500

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -32,7 +32,7 @@ export const getStoredUserName = () => {
 
 export const getUserId = async (userName) => {
   try {
-    const response = await api.get(`api/users/GetUserId`, {params: {userName}});
+    const response = await api.get(`api/user/GetUserId`, {params: {userName}});
     return response.data;
   } catch (error) {
     console.error('Ошибка при получении id:', error.response?.data || error.message);
