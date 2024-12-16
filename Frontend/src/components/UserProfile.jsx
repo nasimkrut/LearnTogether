cd94323b4e02cc9a27b81b85c9a4ae9994153383
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2024-12-16 23:10:54 +0500

    Finally

--- Frontend/src/components/UserProfile.jsx
+++ Frontend/src/components/UserProfile.jsx
@@ -6,33 +6,37 @@ const UserProfile = ({user, onClose}) => {
   const [showContact, setShowContact] = useState(false);
 
   return (
-    <div className="user-profile">
-      <button className="close-button" onClick={onClose} aria-label="Close profile">×</button>
-
-        <div className="profile-content">
-            <img src={user.photo ?? '/placeholder.png'} alt={user.userName} className="user-photo"/>
-            <h2>{user.fullName}</h2>
-            <p>Обо мне: {user.description}</p>
-            {showContact ? (
-                (user.userName ?
-                        <a
-                            href={`https://t.me/${user.userName}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="contact-info"
-                            style={{textDecoration: "none"}}
-                        >
-                            <p>@{user.userName}</p>
-                        </a> : <p>Тут пусто</p>
-                )
-            ) : (
-                <Button onClick={() => {
-                    console.log(`User asked for contact of user ${user.userName}`)
-                    setShowContact(true)
-                }}>Написать в Telegram</Button>
-            )}
-        </div>
-    </div>
+      <div className="user-profile">
+          <button className="close-button" onClick={onClose} aria-label="Close profile">×</button>
+          <div className="profile-image1">
+              <img src={user.photo ?? '/placeholder.png'} alt={user.userName} className="user-photo"/>
+          </div>
+          <div className="profile-content">
+              <h2>{user.fullName}</h2>
+              <>
+                  <h3>Обо мне</h3>
+                  <p>{user.description}</p>
+                  {showContact ? (
+                      (user.userName ?
+                              <a
+                                  href={`https://t.me/${user.userName}`}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                  className="contact-info"
+                                  style={{textDecoration: "none"}}
+                              >
+                                  <p>@{user.userName}</p>
+                              </a> : <p>Тут пусто</p>
+                      )
+                  ) : (
+                      <Button onClick={() => {
+                          console.log(`User asked for contact of user ${user.userName}`)
+                          setShowContact(true)
+                      }}>Написать в Telegram</Button>
+                  )}
+              </>
+          </div>
+      </div>
   )
 }
 
