c394adb65ad690a3974eee44d776ce782e7e7067
nasimkrut <nasimkrut@gmail.com>
2024-12-18 01:22:18 +0500

    no last try

--- LearnTogether/LearnTogether.API/Program.cs
+++ LearnTogether/LearnTogether.API/Program.cs
@@ -1,28 +1,24 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using LearnTogether.API.Extaensions;
 using LearnTogether.Application.Services;
 using LearnTogether.Core.Interfaces;
 using LearnTogether.Infrastructure;
 using Microsoft.AspNetCore.CookiePolicy;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-// builder.Services.AddCors(options =>
-// {
-//     options.AddPolicy("AllowSpecificOrigin",
-//         policy =>
-//         {
-//             policy.WithOrigins("http://learn-together.xyz/") 
-//                 .AllowAnyHeader()
-//                 .AllowAnyMethod()
-//                 .AllowCredentials();
-//         });
-// });
+// удалить, если вдруг что не так
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -36,11 +32,6 @@ builder.Services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IPostService, PostService>();
 builder.Services.AddScoped<ISubscriptionService, SubscriptionService>();
-
-builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("JwtOptions"));
-
-builder.Services.AddApiAuthentication(builder.Services.BuildServiceProvider()
-    .GetRequiredService<IOptions<JwtOptions>>());
 
 builder.Services.AddAuthorization();
 
@@ -62,11 +53,11 @@ using (var scope = app.Services.CreateScope())
 //     MinimumSameSitePolicy = SameSiteMode.Strict,
 //     Secure = CookieSecurePolicy.Always
 // });
+
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
-// app.UseCors("AllowSpecificOrigin"); aaaapt apt aaapt apt aaaapt apt
-
+app.UseCors("AllowAll");
 app.MapControllers();
 
 app.Run();
